eba52b9551b0627cd01ccddcc0a9dad540337ea7
Anna Nescierowicz <annie.odesk@gmail.com>
2022-05-17 19:00:02 +0300

    feat: add solution for 05-merge-styles

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,49 +1,49 @@
-const fs = require('fs');
-const path = require('path');
+const fs = require("fs");
+const path = require("path");
 const fsPromises = fs.promises;
 
-function copyDir(){
-    fs.mkdir(path.resolve(__dirname, 'files-copy'), { recursive: true }, (err) => {
-        if (err) throw err;
-      });
+function copyDir() {
+  fs.mkdir(
+    path.resolve(__dirname, "files-copy"),
+    { recursive: true },
+    (err) => {
+      if (err) throw err;
+    }
+  );
 
-       fs.readdir(path.join(__dirname, 'files-copy'),
+  fs.readdir(
+    path.join(__dirname, "files-copy"),
     { withFileTypes: true },
     (err, files) => {
-        if (err)
-            console.log(err);
-        else {
-            files.forEach(file => {
-                const currentPath = path.join(__dirname, 'files-copy', file.name);
-                fs.unlink(currentPath, (err) => {
-                    if(err) throw err;
-                });
-            })
-        }
+      if (err) console.log(err);
+      else {
+        files.forEach((file) => {
+          const currentPath = path.join(__dirname, "files-copy", file.name);
+          fs.unlink(currentPath, (err) => {
+            if (err) throw err;
+          });
+        });
+      }
     }
-    ) 
+  );
 
-    fs.readdir(path.join(__dirname, 'files'),
+  fs.readdir(
+    path.join(__dirname, "files"),
     { withFileTypes: true },
     (err, files) => {
-        console.log("\nCopying...");
-        if (err)
-            console.log(err);
-        else {
-            files.forEach(file => {
-            const src = path.join(__dirname, 'files', file.name);
-            const dest = path.join(__dirname, 'files-copy', file.name);
-            fs.copyFile(src, dest, () => {
-                console.log(file.name + ' is successfully copied');
-            } );
-        })}
+      console.log("\nCopying...");
+      if (err) console.log(err);
+      else {
+        files.forEach((file) => {
+          const src = path.join(__dirname, "files", file.name);
+          const dest = path.join(__dirname, "files-copy", file.name);
+          fs.copyFile(src, dest, () => {
+            console.log(file.name + " is successfully copied");
+          });
+        });
+      }
     }
-    )
-    
-     
-    
+  );
 }
 
 copyDir();
-
-
---
+++ 05-merge-styles/index.js
@@ -0,0 +1,27 @@
+const path = require("path");
+const fs = require("fs");
+
+let writeableStream = fs.createWriteStream(
+  path.join(__dirname, "project-dist", "bundle.css")
+);
+
+fs.readdir(
+  path.join(__dirname, "styles"),
+  { withFileTypes: true },
+  (err, files) => {
+    console.log("\nAnalazing files...");
+    if (err) console.log(err);
+    else {
+      files.forEach((file) => {
+        if (file.name.endsWith(".css")) {
+          const src = path.join(__dirname, "styles", file.name);
+          const output = path.join(__dirname, "project-dist", "bundle.css");
+          fs.readFile(src, "utf-8", (err, data) => {
+            if (err) throw err;
+            else writeableStream.write(data);
+          });
+        }
+      });
+    }
+  }
+);
